82b4698e4e66cefe8bb6f88fb58cc96af89be653
sswire1 <samuelswire1@gmail.com>
2026-09-11 15:40:52 -0400

    Update to 9-9 data

--- tf/0.0.1-alpha/otext.tf
+++ tf/0.0.1-alpha/otext.tf
@@ -1,11 +1,10 @@
 @config
 @compiler=S.P. Swire
 @converters=S.P. Swire
-@fmt:lex-orig-full={g_lex_utf8}{trailer}
-@fmt:text-cons={g_cons_utf8}{trailer}
-@fmt:text-orig-full={g_voc_utf8}{trailer}
-@fmt:text-trans-cons={g_cons}{trailer}
-@fmt:text-trans-full={g_voc}{trailer}
+@fmt:text-cons={cons_utf8}{trailer}
+@fmt:text-orig-full={voc_utf8}{trailer}
+@fmt:text-trans-cons={cons}{trailer}
+@fmt:text-trans-full={voc}{trailer}
 @institute=ETCBC (Eep Talstra Centre for Bible and Computer)
 @license=MIT License
 @licenseUrl=https://mit-license.org/
@@ -15,7 +14,7 @@
 @source=ETCBC Targum Corpus
 @sourceFormat=XML
 @version=0.0.1-alpha
-@xmlVersion=2026-08-13
+@xmlVersion=2026-09-11
 @writtenBy=Text-Fabric
-@dateWritten=2026-08-13T20:39:15+00:00Z
+@dateWritten=2026-09-11T19:37:16+00:00Z
 
